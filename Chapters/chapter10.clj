0ec50d5b0a9971dc0d820cd1c1a1bfff7905b9e9
Vesa-Matti Salo <jujix@users.noreply.github.com>
2018-02-11 10:37:02 +0200

    Add chapter 10 exercises

---
+++ Chapters/chapter10.clj
@@ -0,0 +1,51 @@
+;Exercise 1
+
+(defn quote-word-count
+  []
+  (let [number-of-quotes (atom 8)]
+  (println (str "The result is first: " @number-of-quotes))
+  (swap! number-of-quotes inc)
+  (swap! number-of-quotes inc)
+(println (str "After swapping: " @number-of-quotes))))
+
+
+
+;Exercise 2
+
+(def quote-url "http://www.braveclojure.com/random-quote")
+
+(defn word-count
+    [wctext]
+    (->> (clojure.string/split wctext #"[ .;?!\-\n\r]")
+        (filter #(not (empty? %)))
+        (map #(clojure.string/lower-case %))
+        (frequencies)))
+
+(defmacro n-futures
+    [n body]
+    `(let [promises# (take ~n (repeatedly promise))]
+        (doseq [p# promises#]
+            (do ~body (deliver p# true)))
+        (every? ~deref promises#)))
+
+(defn random-quote-word-count
+    [num-quotes]
+    (let [wc (atom {})]
+      (do
+        (n-futures num-quotes
+            (swap! wc (fn [current-wc]
+                        (merge-with + current-wc (word-count (slurp quote-url))))))
+        @wc)))
+
+
+;Exercise 3
+
+(defn str-character-state
+  [character]
+  (clojure.string/join " " (map #(str % " => " (% character)) (keys character))))
+
+
+(defn start-game
+  []
+  (let [alliance-knight (ref {:hp 15 :max-hp 40 :healing-potions 0})
+  [horde-orc (ref {:hp 40 :max-hp 40 :healing-potions 1})]]))
